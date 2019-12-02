ecac934b94d17fb6008260e7ad62bd5c520a6610
caterpillar <caterpillarc086@gmail.com>
2019-12-02 01:31:07 -0800

    Add styling and downloading spinner

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,19 @@
 const form = document.getElementById('scrape_form');
+const submitBtn = document.getElementById('submit_btn');
+let loaderContainer = document.getElementById('loader_container');
 
 function urlSubmit(event) {
     event.preventDefault();
 
     let xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-            respMsg = document.createElement('li');
-            respMsg.appendChild(document.createTextNode(JSON.parse(xmlhttp.responseText)['message']));
+            let tempLoader = document.getElementById('loader_toggle');
+            if (tempLoader != null) {
+                loaderContainer.removeChild(tempLoader);
+            }
+            let respMsg = document.createElement('li');
+            respMsg.appendChild(document.createTextNode(JSON.parse(xmlhttp.responseText)['message'].replace(/\\n/g, '')));
             document.getElementById('execution_results').appendChild(respMsg);
         }
     };
@@ -15,4 +21,16 @@ function urlSubmit(event) {
     const url_input = document.getElementById('url');
     xmlhttp.open('GET', '/scrape?' + url_input.name + '=' + url_input.value, true);
     xmlhttp.send();
+}
+
+form.addEventListener('submit', showLoader, false);
+
+function showLoader() {
+    let checkEmpty = document.getElementById('url').value;
+    if (checkEmpty != "" ) {
+        let loader = document.createElement('div');
+        loader.setAttribute('id', 'loader_toggle');
+        loaderContainer.appendChild(loader);
+        loader.className += 'lds-hourglass';
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config')[env];
-const express = require("express");
+const express = require('express');
 const router = express.Router({ mergeParams: true });
 const scrape = require('website-scraper');
 const SaveResourceToExistingDirectoryPlugin = require('website-scraper-existing-directory');
 
-router.get("/", function (req, res) {
-    res.render("pages/landing");
+router.get('/', function (req, res) {
+    res.render('pages/landing');
 });
 
-router.get("/scrape", function (req, res) {
+router.get('/scrape', function (req, res) {
     let url = req.query.url;
     let domain = url.replace(/https?:\/\//i, '').replace('www.', '').split(/[/?#]/)[0];
 
-    const targetDir = `${config.download_directory}/${domain}`;
+    const targetDir = `./${config.download_directory}/${domain}`;
     console.log(`Downloading files from ${url} to directory: ${targetDir}`);
 
     class StatusMonitorPlugin {
@@ -24,7 +24,7 @@ router.get("/scrape", function (req, res) {
     }
     const options = {
         urls: [url],
-        directory: `./${targetDir}`,
+        directory: `${targetDir}`,
         plugins: [ new SaveResourceToExistingDirectoryPlugin(), new StatusMonitorPlugin() ],
         recursive: true,
         maxDepth: 2,
@@ -37,10 +37,10 @@ router.get("/scrape", function (req, res) {
     scrape(options).then((result) => {
         const successMsg = `Resources succesfully downloaded to directory: ${targetDir}`;
         console.log(successMsg);
-        res.send({ "message" : successMsg });
+        res.send({ 'message' : successMsg });
     }).catch((err) => {
         console.log(err);
-        res.status(400).send({ "message" : JSON.stringify(err.cause) });
+        res.status(400).send({ 'message' : JSON.stringify(err.cause) });
     });
 });
 
